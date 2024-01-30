189c97b1a6d713d1c7497722183cb70a0c8266e7
4llves <jhonatab4@gmail.com>
2024-01-29 21:13:22 -0300

    function increment and decrement stepper

--- src/components/Stepper/index.tsx
+++ src/components/Stepper/index.tsx
@@ -1,4 +1,5 @@
 import { Minus, Plus } from '@phosphor-icons/react'
+import { useState } from 'react'
 import { StepperButton, StepperContainer } from './styles'
 
 interface StepperButton {
@@ -6,14 +7,28 @@ interface StepperButton {
 }
 
 export function Stepper({ className }: StepperButton) {
+  const [value, setValue] = useState<number>(0)
+
+  const formatValue = (value: number): string => {
+    return value.toString().padStart(2, '0')
+  }
+
+  const valueIncrement = () => {
+    setValue((value) => value + 1)
+  }
+
+  const valueDecrement = () => {
+    setValue((value) => (value > 0 ? value - 1 : 0))
+  }
+
   return (
     <StepperContainer className={className}>
       <StepperButton>
-        <Minus weight="bold" />
+        <Minus weight="bold" onClick={valueDecrement} />
       </StepperButton>
-      <p>10</p>
+      <p>{formatValue(value)}</p>
       <StepperButton>
-        <Plus weight="bold" />
+        <Plus weight="bold" onClick={valueIncrement} />
       </StepperButton>
     </StepperContainer>
   )
